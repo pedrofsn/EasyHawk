6412d8ba911936b7f3cdac44656583aaaef109c4
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2019-06-27 15:39:33 -0300

    Updating dependencies and migrating to android x

--- app/src/main/java/br/com/redcode/easyhawk/sample/view/MainActivity.kt
+++ app/src/main/java/br/com/redcode/easyhawk/sample/view/MainActivity.kt
@@ -1,7 +1,7 @@
 package br.com.redcode.easyhawk.sample.view
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import br.com.redcode.easyhawk.sample.R
 import br.com.redcode.easyhawk.sample.model.User
 import br.com.redcode.easyhawk.sample.storage.SP
