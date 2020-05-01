fe7a0fde54cbcc9a7e1c79e423f458c0675da74c
pedrofsn <pedrokra@gmail.com>
2020-05-01 11:18:30 -0300

    Improving sample

--- app/src/main/java/br/com/redcode/easyhawk/sample/storage/SP.kt
+++ app/src/main/java/br/com/redcode/easyhawk/sample/storage/SP.kt
@@ -1,8 +1,8 @@
 package br.com.redcode.easyhawk.sample.storage
 
 import br.com.redcode.easyhawk.library.EasyHawk
-import br.com.redcode.easyhawk.sample.model.User
 import br.com.redcode.easyhawk.sample.extensions.toLogcat
+import br.com.redcode.easyhawk.sample.model.User
 
 /**
  * Created by pedrofsn on 17/10/2017.
@@ -15,22 +15,30 @@ object SP : EasyHawk() {
 
     override fun clear() = delete(USER, DESCRIPTION, REMEMBER)
 
-    override fun delete(key: String) {
-        super.delete(key)
-        "Key '$key' deleted".toLogcat()
+    override fun delete(key: String): Boolean {
+        val deleted = super.delete(key)
+        "${getStatusOperation(deleted)} | Key '$key' deleted".toLogcat()
+        return deleted
     }
 
-    override fun save(key: String, value: Any?) {
+    override fun save(key: String, value: Any?): Boolean {
         if (key.isNotBlank()) {
-            when (value) {
+            return when (value) {
                 is User -> persist(key, value)
                 else -> super.save(key, value)
             }
         }
+
+        return false
     }
 
-    override fun persist(key: String, value: Any?) {
-        super.persist(key, value)
-        "Key '$key' saved with '$value'".toLogcat()
+    override fun persist(key: String, value: Any?): Boolean {
+        val saved = super.persist(key, value)
+        "${getStatusOperation(saved)} | Key '$key' saved with '$value'".toLogcat()
+        return saved
+    }
+
+    fun getStatusOperation(success: Boolean): String {
+        return "[Operation is successful: ${if (success) "Yes]" else "Noooooo!]"}"
     }
 }
--- app/src/main/java/br/com/redcode/easyhawk/sample/view/MainActivity.kt
+++ app/src/main/java/br/com/redcode/easyhawk/sample/view/MainActivity.kt
@@ -1,10 +1,15 @@
 package br.com.redcode.easyhawk.sample.view
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import br.com.redcode.easyhawk.sample.R
 import br.com.redcode.easyhawk.sample.model.User
 import br.com.redcode.easyhawk.sample.storage.SP
+import br.com.redcode.easyhawk.sample.storage.SP.DESCRIPTION
+import br.com.redcode.easyhawk.sample.storage.SP.REMEMBER
+import br.com.redcode.easyhawk.sample.storage.SP.USER
+import br.com.redcode.easyhawk.sample.storage.SP.getStatusOperation
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -32,31 +37,39 @@ class MainActivity : AppCompatActivity() {
         // Saving string DESCRIPTION when user press SAVE button
         buttonSaveDescription.setOnClickListener {
             val description = editTextDescription.editableText.toString().trim()
-            SP.save(SP.DESCRIPTION, description)
+            save(DESCRIPTION, description)
         }
 
         // Saving boolean REMEMBER when user change switchcompat status
-        switchCompat.setOnCheckedChangeListener { _, isChecked -> SP.save(SP.REMEMBER, isChecked) }
+        switchCompat.setOnCheckedChangeListener { _, isChecked -> save(REMEMBER, isChecked) }
 
         // Saving object USER when user press SAVE button
         buttonSaveUserModel.setOnClickListener {
             val name = editTextName.editableText.toString().trim()
             val email = editTextEmail.editableText.toString().trim()
-
-            if (name.isBlank()) {
-                editTextName.error = "Empty"
+            val function: (String, EditText) -> Unit = { text, view ->
+                if (text.isBlank()) {
+                    view.error = "Empty field"
+                }
             }
 
-            if (email.isBlank()) {
-                editTextEmail.error = "Empty"
-            }
+            function(name, editTextName)
+            function(email, editTextEmail)
 
             val user = User(
-                    name = name,
-                    email = email
+                name = name,
+                email = email
             )
 
-            SP.save(SP.USER, user)
+            save(USER, user)
         }
     }
+
+    private fun save(tag: String, data: Any?) {
+        val isOperationSuccessful = SP.save(tag, data)
+        val statusOperation = getStatusOperation(isOperationSuccessful)
+        val text = "$statusOperation\nKey: $tag\nData: $data"
+        textViewResult.text = text
+    }
+
 }
